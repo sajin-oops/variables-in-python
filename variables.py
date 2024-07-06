41217e4f5b6f06aa37407448eebca3f4ccabc54e
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-06 17:10:28 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,23 @@
+name = "Sajin"
+print(name)
+print("Hii "+name)
+print(name)
+name = "nijas"
+print(name)
+
+name =222
+print(name)
+name_first ="Aaron"
+print(name_first)
+
+#valid variable name can be
+
+''' - multi line command
+name_first,name_2223, _name
+'''
+
+# invalid variable name can be
+'''
+name#,name$,1name 
+'''
+
